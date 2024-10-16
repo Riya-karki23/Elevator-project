efdde3289dafd68bb04a85c033a8ef83edc1daae
Mackook Air <riyakarki2308@gmail.com>
2024-10-16 09:57:56 +0530

    chnaged lift colour

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ callBtns.forEach((btn,index)=>{
 btn.addEventListener("click",()=>{
 
         btn.classList.add('btn-waiting');
+        liftImage[index].classList.add('icon-red')
+
         btn.innerHTML='waiting';
 
     const buttonRect=btn.getBoundingClientRect();
@@ -33,13 +35,18 @@ const liftPositionY=liftOriginalPosition.top+window.screenY;
 
 if(Math.abs(liftPositionY-positionY)<5){
     console.log('lift is on that floor only');
+    btn.classList.remove('btn-waiting')
     btn.classList.add('btn-arrived');
     btn.innerHTML='Arrived';
+    lift.classList.remove('icon-black')
+    lift.classList.add('icon-green')
     arrivalSound.play();
 
     setTimeout(() => {
+    btn.classList.remove('btn-arrived')
     btn.classList.add('btn-call');
     btn.innerHTML='call';
+    lift.classList.add('icon-black')
     }, 2000);
 
     return;
@@ -54,6 +61,7 @@ lift.style.transform=`translateY(${translateY}px)`;
 setTimeout(() => {
 btn.classList.remove('btn-waiting');
 btn.classList.add('btn-arrived');
+lift.classList.add('icon-green')
 btn.innerHTML='Arrived';
 arrivalSound.play();
 }, 4000);
@@ -61,6 +69,7 @@ arrivalSound.play();
 setTimeout(()=>{
     btn.classList.remove('btn-arrived');
 btn.classList.add('btn-call');
+lift.classList.add('icon-black')
 btn.innerHTML='call';
 },6000)
 
